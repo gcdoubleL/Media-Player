dd0e83ea3f41815f7a282d0999edf572926f6aad
gcdoubleL <117619368+gcdoubleL@users.noreply.github.com>
2022-11-06 23:22:55 -0500

    Media player
    Diff versions + media tests

---
+++ Online Media Player/0.3/script.js
@@ -0,0 +1,31 @@
+const load = document.getElementById('choice');
+load.addEventListener('change', update);
+
+function update(event) {
+    var area = document.getElementById('pick');
+    var newMedia;
+    var fileName = document.getElementById('choice').files[0].name;
+    var fileSrc = URL.createObjectURL(event.target.files[0]);
+    var fileExt = fileName.split('.')[1];
+    if (fileExt === 'jpg' || fileExt === 'png') {
+        newMedia = document.createElement('img');
+        newMedia.type = 'image/*';
+    } else if (fileExt === 'mp4' || fileExt === 'mkv') {
+        newMedia = document.createElement('video');
+        newMedia.type = 'video/*';
+        newMedia.controls = 'controls';
+    } else if (fileExt === 'flac' || fileExt === 'mp3') {
+        newMedia = document.createElement('audio');
+        newMedia.type = 'audio/*';
+        newMedia.controls = 'controls';
+    }
+
+    newMedia.src = fileSrc;
+    newMedia.width = '300';
+    newMedia.height = '300';
+
+    if (area.hasChildNodes()) {
+        area.removeChild(area.firstChild);
+    }
+    document.getElementById('pick').appendChild(newMedia);
+}
